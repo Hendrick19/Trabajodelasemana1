6e472622530e507609c3974852b91d42ac16c5ea
Hendrick19 <u21228076@utp.edu.pe>
2025-03-28 18:03:22 -0500

    Update Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear un arreglo de calificaciones
+        int[] calificaciones = {8, 9, 7, 10, 6};
+
+        // Crear un objeto Alumno
+        Alumno alumno1 = new Alumno("Hendrick", calificaciones);
+
+        // Mostrar información del alumno
+        alumno1.mostrarInfo();
+    }
+}
