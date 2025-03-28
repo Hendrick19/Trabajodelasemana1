6f7f7c174c7cc87c0190711709ca33a05a4dbb46
Hendrick19 <u21228076@utp.edu.pe>
2025-03-28 18:04:42 -0500

    prueba nomas

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class Persona {
     String nombre;
     int edad;
@@ -30,3 +31,37 @@ public void cumplirAños() {
 
  }
 }
+=======
+public class Persona {
+    String nombre;
+    int edad;
+
+ public Persona(String nombre, int edad){
+    this.nombre=nombre;
+    this.edad=edad;
+
+ }
+ public void saludar(){
+    System.out.println("hola, mi nombre es: "+nombre+",y tengo"+edad+"años.");
+
+ }
+  // Método para cambiar el nombre
+  public void cambiarNombre(String nuevoNombre) {
+    nombre = nuevoNombre;
+    System.out.println("Ahora me llamo " + nombre + ".");
+}
+public void cumplirAños() {
+    edad++;
+    System.out.println(nombre  + " ha cumplido años y ahora tiene mas que tu " + edad + " años.");
+}
+
+ public static void main(String[] args) {
+    Persona personal= new Persona("hendrick",23);
+
+    personal.saludar();
+    personal.cambiarNombre("juan");
+    personal.cumplirAños();
+
+ }
+}
+>>>>>>> 7615f1d (prueba nomas)
